b113553be90fb18cccec53ab5a1b1db2f616ba3e
CoDev2158 <CoDev2158@gmail.com>
2025-01-25 20:27:33 -0600

    Added some javascript

---
+++ main.js
@@ -0,0 +1,22 @@
+// How do I find the api online if I don't know where to look? Used GPT Search to help me out here
+//https://api.github.com/users/{username} <-- This is the API Link found.
+
+const form = document.querySelector(".input");
+form.onsubmit = async (event) => {
+  event.preventDefault();
+  const info = new FormData(form);
+  const info2 = info.get("userinput");
+  const userinfo = await getUserInfo(info2);
+  try {
+    const image = document.querySelector("#profile-pic");
+    image.src = userinfo.avatar_url; //"https://avatars.githubusercontent.com/u/81119190?v=4"; //userinfo.avatar_url;
+  } catch {
+    alert("Error fetching user data");
+    console.log("Error fetching user data");
+  }
+};
+async function getUserInfo(username) {
+  const info = await fetch(`https://api.github.com/users/${username}`);
+  const info2 = await info.json();
+  return info2;
+}
